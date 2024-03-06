d8bc09c8309abe6a88c846ec9cbea87a33d04b02
Johannes <theking2@king.ma>
2024-03-06 21:31:29 +0100

    cookie settings

--- inc/session.inc.php
+++ inc/session.inc.php
@@ -1,13 +1,15 @@
 <?php declare(strict_types=1);
 
-session_start([
-	'name' => DEBUG? 'Session-ID': '__Secure-Session-ID',
+session_start( [ 
+	'name' => DEBUG ? 'Session-ID' : '__Secure-Session-ID',
 	'cookie_lifetime' => 0,
 	'cookie_path' => '/',
 	'cookie_domain' => $_SERVER['HTTP_HOST'],
-	'cookie_samesite' => 'Strict',	
+	'cookie_secure' => true,
+	'cookie_httponly' => true,
+	'cookie_samesite' => 'Strict',
 	'sid_length' => 96,
 	'sid_bits_per_character' => 6,
-  'use_strict_mode'=> true,
-  'referer_check'=> $_SERVER['HTTP_HOST'],
-]);
+	'use_strict_mode' => true,
+	'referer_check' => $_SERVER['HTTP_HOST'],
+] );
